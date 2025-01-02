fb60a4d07e54e8834fa11c7e5b94e1dd0f6297d3
Maulid Fajar <67719670+MaulidFajar@users.noreply.github.com>
2025-01-02 19:20:15 +0700

    update on main js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 const burgerBtn = document.querySelector(".burger_btn");
 const navbarList = document.querySelector(".navbar_list");
 const cartIconBtn = document.querySelector(".cart_icon");
+const itemCount = document.querySelector(".item_count");
 const cartBasket = document.querySelector(".cart_basket");
 const productBasket = document.querySelector(".product_basket");
 const deleteBtn = document.querySelector(".delete_btn");
@@ -38,84 +39,21 @@ burgerBtn.addEventListener("click", () => {
   }
 });
 
-productInput.value = 0;
-
 cartIconBtn.addEventListener("click", () => {
   cartBasket.classList.toggle("hidden");
-});
-
-productInput.addEventListener("input", () => {
-  console.log(Number(productInput.value));
 });
 
 incrementBtn.addEventListener("click", () => {
   productInput.value++;
 });
+
 decrementBtn.addEventListener("click", () => {
-  productInput.value == "0"
-    ? decrementBtn.setAttribute(disabled, "true")
-    : productInput.value--;
+  if (productInput.value > 0) {
+    productInput.value--;
+  }
 });
 
-cartBtn.addEventListener("click", () => {
-  productBasket.innerHTML = `<div class="flex items-center justify-center gap-x-4 py-5">
-                    <div class="product_img">
-                      <img
-                        src="assets/images/image-product-1.jpg"
-                        alt=""
-                        width="50"
-                        class="rounded-md"
-                      />
-                    </div>
-                    <div class="product_desc">
-                      <p>Fall Limited Edition Sneakers</p>
-                      <div class="price flex">
-                        <p>$125.00</p>
-                        <span class="mx-1"> x ${productInput.value} </span>
-                        <p>$${125.0 * productInput.value}.00</p>
-                      </div>
-                    </div>
-                    <img
-                      src="assets/images/icon-delete.svg"
-                      alt=""
-                      class="cursor-pointer"
-                      onclick="deleteCartItem()"
-                    />
-                  </div>
-                  <button
-                    class="mb-7 mt-1 w-full flex justify-center items-center bg-orange py-3 text-sm rounded-lg font-extrabold text-dark-blue gap-x-3 transition-all duration-150"
-                  >
-                    Checkout
-                  </button>`;
-});
-
-const deleteCartItem = () => {
-  productBasket.innerHTML = "";
-};
-
-let imgCurrentIndex = 0;
-
-const updateImg = (index) => {
-  const selectedImg = imgThumbnail[index];
-  mainImg.src = selectedImg.src;
-
-  imgThumbnail.forEach((tumbnail) => {
-    tumbnail.parentElement.style.border = "none";
-  });
-  selectedImg.parentElement.style.border = "2px solid hsl(26, 100%, 55%)";
-  selectedImg.parentElement.style.borderRadius = "14px";
-};
-
-const updateLightboxImg = (index) => {
-  const selectedImg = lightboxThumbnail[index];
-  LightboxMainImg.src = selectedImg.src;
-
-  lightboxThumbnail.forEach((tumbnail) => {
-    tumbnail.parentElement.style.border = "none";
-  });
-  selectedImg.parentElement.style.border = "2px solid hsl(26, 100%, 55%)";
-  selectedImg.parentElement.style.borderRadius = "14px";
-};
+cartBtn.addEventListener("click", () => handleProductCart());
 
 mainImg.addEventListener("click", () => {
   lightboxContainer.classList.remove("hidden");
@@ -124,48 +62,143 @@ mainImg.addEventListener("click", () => {
 
 lightboxThumbnail.forEach((img, index) => {
   img.addEventListener("click", () => {
-    updateLightboxImg(index);
-    updateImg(index);
-    imgCurrentIndex = index;
+    handleMainImg(
+      index,
+      lightboxThumbnail,
+      LightboxMainImg,
+      (index, thumbnail, mainImage) => {
+        updateImg(index, thumbnail, mainImage);
+        updateImg(index, imgThumbnail, mainImg);
+      }
+    );
   });
 });
 
 imgThumbnail.forEach((img, index) => {
-  img.addEventListener("click", () => {
-    updateImg(index);
-    updateLightboxImg(index);
-    imgCurrentIndex = index;
-  });
+  img.addEventListener("click", () =>
+    handleMainImg(
+      index,
+      imgThumbnail,
+      mainImg,
+      (index, thumbnail, mainImage) => {
+        updateImg(index, thumbnail, mainImage);
+        updateImg(index, lightboxThumbnail, LightboxMainImg);
+      }
+    )
+  );
 });
 
-prevBtn.addEventListener("click", () => {
-  imgCurrentIndex =
-    (imgCurrentIndex - 1 + imgThumbnail.length) % imgThumbnail.length;
-  updateImg(imgCurrentIndex);
-});
+prevBtn.addEventListener("click", () =>
+  handleImgChange(-1, imgThumbnail, mainImg, updateImg)
+);
 
-nextBtn.addEventListener("click", () => {
-  imgCurrentIndex =
-    (imgCurrentIndex + 1 + imgThumbnail.length) % imgThumbnail.length;
-  updateImg(imgCurrentIndex);
-});
+nextBtn.addEventListener("click", () =>
+  handleImgChange(1, imgThumbnail, mainImg, updateImg)
+);
 
-lightboxPrevBtn.addEventListener("click", () => {
-  imgCurrentIndex =
-    (imgCurrentIndex - 1 + lightboxThumbnail.length) % lightboxThumbnail.length;
-  updateLightboxImg(imgCurrentIndex);
-  updateImg(imgCurrentIndex);
-});
+lightboxPrevBtn.addEventListener("click", () =>
+  handleImgChange(
+    -1,
+    lightboxThumbnail,
+    LightboxMainImg,
+    (index, thumbnail, mainImage) => {
+      updateImg(index, thumbnail, mainImage);
+      updateImg(index, imgThumbnail, mainImg);
+    }
+  )
+);
 
 lightboxNextBtn.addEventListener("click", () => {
-  imgCurrentIndex =
-    (imgCurrentIndex + 1 + lightboxThumbnail.length) % lightboxThumbnail.length;
-  updateLightboxImg(imgCurrentIndex);
-  updateImg(imgCurrentIndex);
+  handleImgChange(
+    1,
+    lightboxThumbnail,
+    LightboxMainImg,
+    (index, thumbnail, mainImage) => {
+      updateImg(index, thumbnail, mainImage);
+      updateImg(index, imgThumbnail, mainImg);
+    }
+  );
 });
 
 lightboxClsBtn.addEventListener("click", () => {
   lightboxContainer.classList.add("hidden");
 });
 
-updateImg(0);
+const updateIndex = (currentIndex, step, lengthImage) => {
+  return (currentIndex + step + lengthImage) % lengthImage;
+};
+
+const handleImgChange = (step, thumbnail, mainImage, update) => {
+  imgCurrentIndex = updateIndex(imgCurrentIndex, step, thumbnail.length);
+  update(imgCurrentIndex, thumbnail, mainImage);
+};
+
+const handleMainImg = (index, img, main, mainimgUpdate) => {
+  mainimgUpdate(index, img, main);
+};
+
+const updateImg = (index, thumbnailImg, mainImage) => {
+  const selectedImg = thumbnailImg[index];
+  mainImage.src = selectedImg.src;
+  thumbnailImg.forEach((thumb) => {
+    thumb.parentElement.style.border = "none";
+    thumb.classList.remove("opacity-50");
+  });
+  selectedImg.parentElement.style.border = "2px solid hsl(26, 100%, 55%)";
+  selectedImg.parentElement.style.borderRadius = "14px";
+  selectedImg.classList.add("opacity-50");
+  imgCurrentIndex = index;
+};
+
+const handleProductCart = () => {
+  if (productInput.value > 0) {
+    productBasket.innerHTML = `<div class="flex items-center justify-center gap-x-4 py-5">
+    <div class="product_img">
+      <img
+        src="assets/images/image-product-1.jpg"
+        alt=""
+        width="50"
+        class="rounded-md"
+      />
+    </div>
+    <div class="product_desc">
+      <p class="text-dark-grayish-blue">Fall Limited Edition Sneakers</p>
+      <div class="price flex">
+        <p class="text-dark-grayish-blue">$125.00</p>
+        <span class="mx-1 text-dark-grayish-blue"> x ${
+          productInput.value
+        } </span>
+        <p class="font-bold">$${125.0 * productInput.value}.00</p>
+      </div>
+    </div>
+    <img
+      src="assets/images/icon-delete.svg"
+      alt=""
+      class="cursor-pointer"
+      onclick="deleteCartItem()"
+    />
+  </div>
+  <button
+    class="mb-7 mt-1 w-full flex justify-center items-center bg-orange hover:bg-orange/50 py-3 text-sm rounded-lg font-extrabold text-dark-blue gap-x-3 transition-all duration-150"
+  >
+    Checkout
+  </button>`;
+    itemCount.classList.remove("hidden");
+    itemCount.innerHTML = productInput.value;
+    productInput.value = 0;
+  }
+};
+
+const deleteCartItem = () => {
+  productBasket.innerHTML = `<div className="flex items-center justify-center w-full p-5 h-40">
+    <span class="font-bold text-dark-grayish-blue">
+        Your cart is empty.
+    </span>
+  </div>`;
+  itemCount.textContent = 0;
+  itemCount.classList.add("hidden");
+};
+
+updateImg(0, lightboxThumbnail, LightboxMainImg);
+updateImg(0, imgThumbnail, mainImg);
+productInput.value = 0;
